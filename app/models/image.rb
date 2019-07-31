63bfcbafbb41cd7b4e1a485b5a255f4fc76d45b5
Frank Cheng <xinnan.cheng@appfolio.com>
2019-07-31 15:38:50 -0700

    FC - Rubocop Style Fix

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,5 +1,5 @@
 class Image < ApplicationRecord
   validates :link, presence: true,
-            format: { with: URI::regexp,
-                      message: "only allows valid URL" }
+                   format: { with: URI::DEFAULT_PARSER.make_regexp,
+                             message: 'only allows valid URL' }
 end
