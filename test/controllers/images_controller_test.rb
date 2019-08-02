f32347d5265c521d8a032bd5f9593749226c1e36
Frank Cheng <xinnan.cheng@appfolio.com>
2019-08-01 17:22:16 -0700

    FC - Image Index +tag

--- test/controllers/images_controller_test.rb
+++ test/controllers/images_controller_test.rb
@@ -42,14 +42,20 @@ class ImageControllerTest < ActionDispatch::IntegrationTest
   end
 
   def test_index
-    Image.create!(link: 'http://valid1.com')
-    Image.create!(link: 'http://valid2.com')
+    Image.create!(link: 'http://valid1.com', tag_list: '1a, 1b')
+    Image.create!(link: 'http://valid2.com', tag_list: '2a, 2b')
     get images_path
     assert_response :ok
 
-    assert_select 'img' do |elements|
-      elements.each_with_index do |element, index|
-        assert_equal element[:src], "http://valid#{2 - index}.com"
+    assert_select 'img' do |images|
+      images.each_with_index do |image, index|
+        assert_equal image[:src], "http://valid#{2 - index}.com"
+      end
+    end
+
+    assert_select 'div.tags' do |tags|
+      tags.each_with_index do |tag, index|
+        assert_includes tag.to_s, "#{2 - index}a, #{2 - index}b"
       end
     end
   end
