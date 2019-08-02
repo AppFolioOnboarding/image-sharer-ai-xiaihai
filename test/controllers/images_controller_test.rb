e6d1055e9983cf43a81743d29794662645ac810c
Frank Cheng <xinnan.cheng@appfolio.com>
2019-08-02 14:13:28 -0700

    FC - Index View tag seperate div
    Place each tag in an individual div, easier for adding links to each tag

--- test/controllers/images_controller_test.rb
+++ test/controllers/images_controller_test.rb
@@ -42,20 +42,20 @@ class ImageControllerTest < ActionDispatch::IntegrationTest
   end
 
   def test_index
-    Image.create!(link: 'http://valid1.com', tag_list: '1a, 1b')
-    Image.create!(link: 'http://valid2.com', tag_list: '2a, 2b')
+    Image.create!(link: 'http://valid1.com', tag_list: '2, 3')
+    Image.create!(link: 'http://valid0.com', tag_list: '0, 1')
     get images_path
     assert_response :ok
 
     assert_select 'img' do |images|
       images.each_with_index do |image, index|
-        assert_equal image[:src], "http://valid#{2 - index}.com"
+        assert_equal image[:src], "http://valid#{index}.com"
       end
     end
 
-    assert_select 'div.tags' do |tags|
+    assert_select 'div.tag' do |tags|
       tags.each_with_index do |tag, index|
-        assert_includes tag.to_s, "#{2 - index}a, #{2 - index}b"
+        assert_includes tag.to_s, index.to_s
       end
     end
   end
