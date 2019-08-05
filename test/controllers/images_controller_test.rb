86f4601d9f8e3d59a81ff03b8629ab9abc8827bf
Frank Cheng <xinnan.cheng@appfolio.com>
2019-08-05 09:57:50 -0700

    FC - Fix Image New test route

--- test/controllers/images_controller_test.rb
+++ test/controllers/images_controller_test.rb
@@ -2,12 +2,12 @@ require 'test_helper'
 
 class ImageControllerTest < ActionDispatch::IntegrationTest
   def test_new
-    get root_path
+    get new_image_path
     assert_response :ok
   end
 
   def test_new_view_body
-    get root_path
+    get new_image_path
     assert_select 'p', 'New Image Submission Page'
   end
 
