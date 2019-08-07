a7a65c1012bef33618ea7dfc9b9d9e8b31cadd72
Frank Cheng <xinnan.cheng@appfolio.com>
2019-08-07 09:56:52 -0700

    FC - JS App using ReactStrap

--- app/javascript/image_sharing/components/App.js
+++ app/javascript/image_sharing/components/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
+import { Container, Row, Col } from 'reactstrap';
+
 import Header from './Header';
 
 export default function App() {
   return (
-    <div>
-      <Header title="Tell us what you think" />
-      Put your components here: Flash Message, Form, Footer
-    </div>
+    <Container>
+      <Row>
+        <Col xs='3'> </Col>
+        <Col xs='6'>
+          <Header title='Tell us what you think' />
+        </Col>
+      </Row>
+    </Container>
   );
 }
-
-/* TODO: Add Prop Types check*/
--- app/javascript/image_sharing/test/components/App.spec.js
+++ app/javascript/image_sharing/test/components/App.spec.js
@@ -1,13 +1,15 @@
 /* eslint-env mocha */
 
 import assert from 'assert';
-import { mount } from 'enzyme';
+import { shallow } from 'enzyme';
 import React from 'react';
 import App from '../../components/App';
 
 describe('<App />', () => {
   it('should render correctly', () => {
-    const wrapper = mount(<App />);
-    assert(wrapper.contains('Tell us what you think'));
+    const wrapper = shallow(<App />);
+
+    assert.strictEqual(wrapper.find('Header').length, 1);
+    assert.strictEqual(wrapper.find('Header').prop('title'), 'Tell us what you think');
   });
 });
