296387e922a622d34af4dc9f7863722534fe1c82
Frank Cheng <xinnan.cheng@appfolio.com>
2019-08-07 14:52:24 -0700

    FC - api Feedback controller create

--- app/controllers/api/feedbacks_controller.rb
+++ app/controllers/api/feedbacks_controller.rb
@@ -1,7 +1,12 @@
 module Api
   class FeedbacksController < ApplicationController
     def create
-      # Implement your create action
+      feedback_new = Feedback.new(name: params[:feedback][:name], comment: params[:feedback][:comment])
+      if feedback_new.valid? && feedback_new.save
+        render json: {}, status: 200
+      else
+        render json: {}, status: 422
+      end
     end
   end
 end
